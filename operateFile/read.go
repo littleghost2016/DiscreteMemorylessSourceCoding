9a575fe8aba08dd0bd31d9020a2118d78e503531
littleghost2016 <785340571@qq.com>
2020-03-27 14:49:03 +0800

    fihish ReadText function

--- operateFile/read.go
+++ operateFile/read.go
@@ -1,1 +1,26 @@
-package operateFile
+package operatefile
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// ReadText 从文件读取内容
+func ReadText(filePath string) []uint8 {
+	inputFile, err := os.Open(filePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "os.Open() failed.")
+		return nil
+	}
+
+	defer inputFile.Close()
+
+	text, err := ioutil.ReadAll(inputFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ioutil.ReadAll() failed.")
+		return nil
+	}
+
+	return text
+}
