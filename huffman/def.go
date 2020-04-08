d2462117b3f668e71b60ca16d4c211c477ad1eb3
littleghost2016 <785340571@qq.com>
2020-04-08 22:14:59 +0800

    add definition of TreeNode and TreeNodeSlice

---
+++ huffman/def.go
@@ -0,0 +1,14 @@
+package huffman
+
+// TreeNode 树节点
+type TreeNode struct {
+	Character rune      `json:"character"` //字符
+	Weight    int       `json:"weight"`    //权重
+	FNode     *TreeNode `json:"fnode"`     //父节点
+	LNode     *TreeNode `json:"lnode"`     //左节点
+	RNode     *TreeNode `json:"rnode"`     //右节点
+	Code      string    `json:"code"`      // 编码
+}
+
+// TreeNodeSlice 树节点切片
+type TreeNodeSlice []*TreeNode
