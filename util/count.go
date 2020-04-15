8f4b252fea2096083578eb733d6d356b02591053
littleghost2016 <785340571@qq.com>
2020-04-15 19:01:44 +0800

    only read byte rather rune now...

--- util/count.go
+++ util/count.go
@@ -1,9 +1,8 @@
 package util
 
 // CountCharacter 统计自个字符的出现次数
-func CountCharacter(text string) (characterFrequencyMap map[rune]int) {
-	// func CountCharacter(text string) map[rune]int {
-	characterFrequencyMap = make(map[rune]int)
+func CountCharacterFromText(text []byte) (characterFrequencyMap map[byte]int) {
+	characterFrequencyMap = make(map[byte]int)
 
 	for _, eachCharacter := range text {
 		if _, ok := characterFrequencyMap[eachCharacter]; ok {
